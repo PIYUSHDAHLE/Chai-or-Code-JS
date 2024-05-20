40420c873505ee47e8745055bbd10204876be95f
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-05-20 17:03:06 +0530

    Data types

---
+++ 03-dataTypes.js
@@ -0,0 +1,10 @@
+// num str bool symbol bigInt undefine null
+
+console.log(typeof(22))
+console.log(typeof("Piyush Dahle"))
+console.log(typeof(true))
+console.log(typeof(Symbol()))
+console.log(typeof(BigInt(45652)))
+console.log(typeof(undefined))
+console.log(typeof(null))
+
