a60ec514f6bfdd59dfbe27f02084b61426e25b4a
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-05-20 15:35:45 +0530

    Basics

---
+++ 01-BASICS.js
@@ -0,0 +1,9 @@
+console.log("Hello Boss");
+let a = "55";
+let b = 55;
+let c = "11";
+console.log(`${typeof(a)}`);
+console.log(`${typeof(b)}`);
+console.log(`${typeof(c)}`);
+console.log(`${a} + ${b} = ${a+b}`);
+console.log(`${a} + ${c} = ${a+c}`);
