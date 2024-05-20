78af2fdd072b6e0a94333f461edc2d5ab38288fc
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-05-20 17:32:07 +0530

    data type Converaion

---
+++ 04-coversion.js
@@ -0,0 +1,14 @@
+let a = "23abc"
+let b = "50" //truecls
+let empty = "" //false
+let zero = 0 //false
+let one = "1" //true
+let n = 55
+
+console.log(Number(a));
+console.log(Number(b));
+console.log(Boolean(b));
+console.log(Boolean(empty));
+console.log(Boolean(zero));
+console.log(Boolean(one));
+console.log(String(n));
