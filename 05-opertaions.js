3c09bbc125735811b595dcd8409b1ba0824c461c
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-05-20 18:00:42 +0530

    operation

---
+++ 05-opertaions.js
@@ -0,0 +1,20 @@
+let a = 25; 
+console.log(a)
+console.log(a++)
+console.log(a++)
+console.log(++a)
+console.log(++a)
+console.log(--a)
+console.log(--a)
+console.log(a--)
+console.log(a--)
+
+
+console.log(2+4)
+console.log(2-4)
+console.log(2*4)
+console.log(2/4)
+console.log(2**4)
+console.log(2%4)
+
+
