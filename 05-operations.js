ff809b11791d7a1d2a0d81f2bbcac24a190f18eb
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-05-22 21:58:38 +0530

    opertions update

--- 05-operations.js
+++ 05-operations.js
@@ -21,3 +21,9 @@ console.log(2%4)
 // its check value and datatypes === 
 // its check only value == 
 
+console.log(2+2+"2") 
+console.log("2"+2+2)
+console.log(2+"2"+2)
+
+// if string/number is first its work as string/number 
+
